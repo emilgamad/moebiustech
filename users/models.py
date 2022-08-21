e54aa0cc4ad1a4fc6e37d62bed2039fed40253af
emilgamad <emilgamad@DESKTOP-OB2GLOR.localdomain>
2022-08-21 18:01:58 +0800

    Updated Contractor model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from app.models import Product
+from app.models import Product, Choice
 
 
 class Client(models.Model):
@@ -15,14 +15,21 @@ class Client(models.Model):
 
 
 class Contractor(models.Model):
-    logo = models.ImageField(blank=True)
-    name = models.CharField(max_length=200)
-    zip_code = models.CharField(max_length=200)
+    experties = models.ManyToManyField(Choice)
     product = models.ManyToManyField(Product)
+    company_name = models.CharField(max_length=200)
+    company_person = models.CharField(max_length=200)
+    field = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+    zip_code = models.CharField(max_length=200)
+    email = models.EmailField(Choice)
+    about = models.CharField(max_length=500)
+    logo = models.ImageField(blank=True)
     pub_date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-        return "{}".format(self.name)
+        return "{}".format(self.company_name)
 
 
 class Reviews(models.Model):
